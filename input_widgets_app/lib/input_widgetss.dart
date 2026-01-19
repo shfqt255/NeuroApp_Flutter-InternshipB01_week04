49c3a8a4c67a338a158f17eb68e84e23a034243b
shfqt255 <shfqt25@gmail.com>
2026-01-19 23:30:49 +0500

    Task 4.2 Input Widgets App Updated, to show the selected data by the user

--- input_widgets_app/lib/input_widgetss.dart
+++ input_widgets_app/lib/input_widgetss.dart
@@ -20,7 +20,7 @@ class SurveyFormState extends State<SurveyForm> {
   // SwitchListTile
   bool notifications = false;
 
-    final List<String> countries = ['Pakistan', 'India', 'Iran', 'Iraq'];
+  final List<String> countries = ['Pakistan', 'India', 'Iran', 'Iraq'];
   String selectedCountry = 'Pakistan';
 
   // Slider
@@ -29,9 +29,9 @@ class SurveyFormState extends State<SurveyForm> {
   String summary = '';
 
   void showSummary() {
-    setState(() {  
-      summary =     
-      //   these ''' colons are used to add multiples lines text; 
+    setState(() {
+      summary =
+          //   these ''' colons are used to add multiples lines text;
           '''
 Sports: $sports
 Music: $music
@@ -53,90 +53,98 @@ Age: ${age.toInt()}
         backgroundColor: Colors.lightBlue,
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            // 3. Implement Checkbox widgets with onChanged
-             Text('Hobbies'),
-            CheckboxListTile(
-              title:  Text('Sports'),
-              value: sports,
-              onChanged: (value) => setState(() => sports = value!),
-            ),
-            CheckboxListTile(
-              title:  Text('Music'),
-              value: music,
-              onChanged: (value) => setState(() => music = value!),
-            ),
-            CheckboxListTile(
-              title:  Text('Technology'),
-              value: tech,
-              onChanged: (value) => setState(() => tech = value!),
-            ),
-             const Divider(),
-             Text('Gender', style: TextStyle(fontSize: 18)),
-        RadioListTile<String>(
-  title: Text('Male'),
-  value: 'Male',
-  groupValue: gender,
-  onChanged: (value) => setState(() => gender = value),
-),
-RadioListTile<String>(
-  title: Text('Female'),
-  value: 'Female',
-  groupValue: gender,
-  onChanged: (value) => setState(() => gender = value),
-),
-RadioListTile<String>(
-  title: Text('Other'),
-  value: 'Other',
-  groupValue: gender,
-  onChanged: (value) => setState(() => gender = value),
-),
-     const Divider(),
-            SwitchListTile(title: Text('Notification'),  value: notifications , onChanged: (value)=>setState(()=>notifications=value)),
-          const Divider(),
-          
-          Text('Country', style: TextStyle(fontSize: 18)),
-
-            DropdownButton<String>(
-              value: selectedCountry,
-              isExpanded: true,
-              items: countries
-                  .map((c) => DropdownMenuItem(value: c, child: Text(c)))
-                  .toList(),
-              onChanged: (v) => setState(() => selectedCountry = v!),
-            ),
-
-            const Divider(),
-            Text('Age', style: TextStyle(fontSize: 18)),
-
-            Slider(
-              value: age,
-              min: 10,
-              max: 60,
-              divisions: 50,
-              label: age.toInt().toString(),
-              onChanged: (value) => setState(() => age = value),
-            ),
-
-            const SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: showSummary,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.lightBlue,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(0),
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                ),
-                child: Text('Show Summary'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              // 3. Implement Checkbox widgets with onChanged
+              Text('Hobbies', style: TextStyle(fontSize: 18)),
+              CheckboxListTile(
+                title: Text('Sports'),
+                value: sports,
+                onChanged: (value) => setState(() => sports = value!),
               ),
-            ),  
-          ],
+              CheckboxListTile(
+                title: Text('Music'),
+                value: music,
+                onChanged: (value) => setState(() => music = value!),
+              ),
+              CheckboxListTile(
+                title: Text('Technology'),
+                value: tech,
+                onChanged: (value) => setState(() => tech = value!),
+              ),
+              const Divider(),
+              Text('Gender', style: TextStyle(fontSize: 18)),
+              RadioListTile<String>(
+                title: Text('Male'),
+                value: 'Male',
+                groupValue: gender,
+                onChanged: (value) => setState(() => gender = value),
+              ),
+              RadioListTile<String>(
+                title: Text('Female'),
+                value: 'Female',
+                groupValue: gender,
+                onChanged: (value) => setState(() => gender = value),
+              ),
+              RadioListTile<String>(
+                title: Text('Other'),
+                value: 'Other',
+                groupValue: gender,
+                onChanged: (value) => setState(() => gender = value),
+              ),
+              const Divider(),
+              SwitchListTile(
+                title: Text('Notification'),
+                value: notifications,
+                onChanged: (value) => setState(() => notifications = value),
+              ),
+              const Divider(),
+
+              Text('Country', style: TextStyle(fontSize: 18)),
+
+              DropdownButton<String>(
+                value: selectedCountry,
+                isExpanded: true,
+                items: countries
+                    .map((c) => DropdownMenuItem(value: c, child: Text(c)))
+                    .toList(),
+                onChanged: (v) => setState(() => selectedCountry = v!),
+              ),
+
+              const Divider(),
+              Text('Age', style: TextStyle(fontSize: 18)),
+
+              Slider(
+                value: age,
+                min: 10,
+                max: 60,
+                divisions: 50,
+                label: age.toInt().toString(),
+                onChanged: (value) => setState(() => age = value),
+              ),
+
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: showSummary,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(0),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  ),
+                  child: Text('Show Summary'),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(summary),
+            ],
+          ),
         ),
       ),
     );
